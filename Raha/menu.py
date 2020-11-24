f6a436e1e42d5204887a2c0f1e3b4918675a4b81
raha <rrrr@gmail.com>
2020-11-24 21:48:02 +0600

    saving the name and type of the Font

--- Raha/menu.py
+++ Raha/menu.py
@@ -12,6 +12,14 @@ from pygame_menu import sound
 import pygame_menu
 from pygame import mixer
 
+
+def data_fun():
+    filename = 'levels/names.txt'
+    data = menu.get_input_data()
+    for k in data.keys():
+        l=data[k]
+    with open(filename, 'w') as file_object:
+        file_object.write(l)
 pygame.init()  # инициализировал миксер
 mixer.music.load('jr.mp3')
 mixer.music.play(0, 20)  # loop
@@ -25,7 +33,9 @@ H_SIZE = 600
 W_SIZE = 800
 pygame.init()
 HELP = " TROIA  TEAM"
-mytheme = pygame_menu.themes.Theme(background_color=(0, 0, 0, 0),  # transparent background
+
+font = pygame_menu.font.FONT_8BIT
+mytheme = pygame_menu.themes.Theme(widget_font=font,background_color=(0, 0, 0, 0),  # transparent background
                 title_shadow=True,
                 title_background_color=(4, 47, 126))
 myimage = pygame_menu.baseimage.BaseImage(
@@ -37,38 +47,41 @@ surface = pygame.display.set_mode((800, 600))
 bg=pygame.image.load("title2.png")
 menu = pygame_menu.Menu(600, 800, 'STORM GLASS', theme=mytheme)
 menu.set_sound(engine, recursive=True)  # добавляем звук клика мыши в основное меню
-menu.add_text_input('Имя :', font_color=(0,252,255),shadow_color=(0,0,0),shadow=True, default='Игрок',background_color=(0, 0, 0,0))
-menu.add_button(' Выход ', pygame_menu.events.EXIT,font_color=(0,252,255),shadow_color=(0,0,0),shadow=True,background_color=(0,0,0,0))
+menu.add_text_input('NAME ', font_color=(0,252,255),shadow_color=(255,0,0),shadow=True, default='PLAYER',background_color=(0, 0, 0,0))
+menu.add_button('SAVE NAME', data_fun,font_color=(255,0,0),shadow_color=(0,0,0,0),shadow=True,background_color=(0,0,0,0))
 
-about_menu = pygame_menu.Menu(600, 800, "О нас", theme=mytheme)
+about_menu = pygame_menu.Menu(600, 800, "ABOUT", theme=mytheme)
 PATH = os.path.join(os.path.dirname(pygame_menu.__file__),
                     'resources', 'images', 'pygame_menu.png')
-menu.add_button(about_menu.get_title(), about_menu, font_color=(0,252,255),shadow_color=(0,0,0),shadow=True,background_color=(0,0,0,0))
-about_menu.add_label('Tokumtayev Rakhat',font_color=(0,252,255),shadow_color=(0,0,0),shadow=True)
+menu.add_button(about_menu.get_title(), about_menu, font_color=(0,252,255),shadow_color=(255,0,0),shadow=True,background_color=(0,0,0,0))
+about_menu.add_label('Tokumtayev Rakhat',font_color=(0,252,255),shadow_color=(255,0,0),shadow=True)
 about_menu.add_image('rake.png', scale=(0.23, 0.23), scale_smooth=True)
 about_menu.add_vertical_margin(50)
-about_menu.add_label('Iztay Almaz',font_color=(0,252,255),shadow_color=(0,0,0),shadow=True)
+about_menu.add_label('Iztay Almaz',font_color=(0,252,255),shadow_color=(255,0,0),shadow=True)
 about_menu.add_image('aleke.png',  scale=(1, 1))
 about_menu.add_vertical_margin(50)
-about_menu.add_label('Igen Arystan',font_color=(0,252,255),shadow_color=(0,0,0),shadow=True)
+about_menu.add_label('Igen Arystan',font_color=(0,252,255),shadow_color=(255,0,0),shadow=True)
 about_menu.add_image('areke.jpg',angle=90,  scale=(0.15, 0.15), scale_smooth=True)
 about_menu.add_vertical_margin(50)
-about_menu.add_label('Zhenis Nursultan',font_color=(0,252,255),shadow_color=(0,0,0),shadow=True)
+about_menu.add_label('Zhenis Nursultan',font_color=(0,252,255),shadow_color=(255,0,0),shadow=True)
 about_menu.add_image('nureke.png',  scale=(0.3, 0.3), scale_smooth=True)
 
-color_menu = pygame_menu.Menu(600, 800, "Персонаж", theme=mytheme)
-menu.add_button(color_menu.get_title(), color_menu, font_color=(0,252,255),shadow_color=(0,0,0),shadow=True,
+color_menu = pygame_menu.Menu(600, 800, "HERO", theme=mytheme)
+menu.add_button(color_menu.get_title(), color_menu, font_color=(0,252,255),shadow_color=(255,0,0),shadow=True,
                 background_color=(0,0,0,0))
 color_menu.set_sound(engine, recursive=True)
 # color_menu.add_button(' Красный ', pygame_menu.events.BACK,shadow=True ,shadow_color=(0, 0, 100),background_color=(255,255,255))
 # color_menu.add_button(' Серый ', pygame_menu.events.BACK,shadow=True,shadow_color=(0, 0, 100), background_color=(255,255,255))
 # color_menu.add_button(' Зеленый ', pygame_menu.events.BACK,shadow=True,shadow_color=(0, 0, 100), background_color=(255,255,255))
-color_menu.add_button(' Синий ', pygame_menu.events.BACK, font_color=(0,252,255),shadow_color=(0,0,0),shadow=True,
+color_menu.add_button(' BLUE ', pygame_menu.events.BACK, font_color=(0,252,255),shadow_color=(255,0,0),font_size=50,shadow=True,
                       background_color=(0,0,0,0))
-color_menu.add_label('Другие цвета будут ',font_color=(0,252,255),shadow_color=(0,0,0),shadow=True)
+color_menu.add_label('other colors will be ',font_color=(0,0,0,0),font_size=20,shadow_color=(255,255,255),shadow=True)
 about_menu.add_vertical_margin(8)
-color_menu.add_label(' доступны в версии ',font_color=(0,252,255),shadow_color=(0,0,0),shadow=True)
+color_menu.add_label(' available in version',font_color=(0,0,0,0),font_size=20,shadow_color=(255,255,255),shadow=True)
 about_menu.add_vertical_margin(8)
-color_menu.add_label('  V 2.0 ',font_color=(0,252,255),shadow_color=(0,0,0),shadow=True)
-menu.add_label(HELP, max_char=-1, font_size=20, margin=(1, 0),font_color=(0,252,255),shadow_color=(0,0,0),shadow=True)
+color_menu.add_label('   23430 ',font_color=(0,0,0,0),font_size=20,shadow_color=(255,255,255),shadow=True)
+menu.add_label(HELP, max_char=-1, font_size=20, margin=(1, 0),font_color=(0,252,255),shadow_color=(255,0,0),shadow=True)
+menu.add_vertical_margin(15)
+menu.add_button(' EXIT ', pygame_menu.events.EXIT,font_color=(255,0,0),shadow_color=(0,0,0,0),shadow=True,background_color=(0,0,0,0))
 menu.mainloop(surface)
+
